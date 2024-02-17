398bab17146b3d0052985373ff50237dfd90a8f8
Jan Florjanczyk <janflorjanczyk@Jans-MacBook-Pro-2.local>
2024-02-17 13:28:26 -0800

    Single pane working

--- observablehq.config.ts
+++ observablehq.config.ts
@@ -20,8 +20,8 @@ export default {
   // theme: "default", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
   // footer: "Built with Observable.", // what to show in the footer (HTML)
-  // toc: true, // whether to show the table of contents
-  // pager: true, // whether to show previous & next links in the footer
+  toc: false, // whether to show the table of contents
+  pager: false, // whether to show previous & next links in the footer
   // root: "docs", // path to the source root for preview
   // output: "dist", // path to the output root for build
 };
