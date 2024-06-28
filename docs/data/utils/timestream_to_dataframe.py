5eca076f030acd030bed63eff495df4423970268
Jan Florjanczyk <janflorjanczyk@Jans-MacBook-Pro-2.local>
2024-06-27 18:16:24 -0700

    Paginate results

--- docs/data/utils/timestream_to_dataframe.py
+++ docs/data/utils/timestream_to_dataframe.py
@@ -1,20 +1,34 @@
 import pandas as pd
 
-def timestream_query_to_dataframe(client, QueryString):
-    timestream_response = client.query(
+def timestream_query_to_dataframe(client, QueryString, MaxRows=100):
+    initial_response = client.query(
         QueryString=QueryString,
-        MaxRows=1000
+        MaxRows=MaxRows
     )
-    rows = timestream_response['Rows']
-    column_names = timestream_response['ColumnInfo']
+
+    NextToken = initial_response.get('NextToken', None)
+
+    responses = [initial_response]
+
+    while NextToken:
+        timestream_response = client.query(
+            QueryString=QueryString,
+            MaxRows=MaxRows,
+            NextToken=NextToken
+        )
+        responses.append(timestream_response)
+        NextToken = timestream_response.get('NextToken', None)
+
     output_frame = (
         pd
         .DataFrame([
             {
                 col['Name']: val['ScalarValue']
-                for col, val in zip(column_names, row['Data'])
+                for col, val in zip(response['ColumnInfo'], row['Data'])
             }
-            for row in rows
+            for response in responses
+            for row in response['Rows']
         ])
     )
+
     return output_frame
